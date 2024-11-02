fd867c9b4d6495e72dac1092f7b49322996667fb
Darren Bono <dbono215@gmail.com>
2024-11-02 11:52:07 -0400

    Add examples

---
+++ examples/helpers/device/device_helper_simple.py
@@ -0,0 +1,46 @@
+from requests.exceptions import HTTPError, RequestException
+
+from cisco_nso_restconf.client import NSORestconfClient
+from cisco_nso_restconf.devices import Devices
+
+# initialize the NSORestconfClient
+client = NSORestconfClient(
+    scheme="http",
+    address="localhost",
+    port=8080,
+    timeout=10,
+    username="admin",
+    password="admin",
+)
+
+# initialize the Devices class
+devices_helper = Devices(client)
+
+# fetch device ned id's
+try:
+    device_ned_ids = devices_helper.get_device_ned_ids()
+    print("---DEVICE NED IDS---")
+    print(device_ned_ids)
+    print()
+
+    device_groups = devices_helper.get_device_groups()
+    print("---DEVICE GROUPS---")
+    print(device_groups)
+    print()
+
+    dc01leaf01_platform = devices_helper.get_device_platform("dc01leaf01")
+    print("---DEVICE PLATFORM---")
+    print(dc01leaf01_platform)
+    print()
+
+except ValueError as val_err:
+    print(f"Value error occurred: {val_err}")
+except HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+except RequestException as err:
+    print(f"Other error occurred: {err}")
+except Exception as e:
+    print(f"Other error occurred: {e}")
+finally:
+    # always close the client session
+    client.close()
